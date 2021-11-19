2ab092a30aa3486a2924ce5fa8f98e66f8d1937f
vGebs <vaughn.geber33@gmail.com>
2021-11-18 20:00:24 -0600

    update the auth(login/signup) view

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -6,6 +6,12 @@ const main = (req, res) => {
     const failurePath = "/auth"
 
     if (req.isAuthenticated()){
+        //After user is authenticated, we need to:
+        //  1. Fetch all events for that user
+        //  2. render the calendar view and add the events as input
+        //  3. If the fetch fails, we will render the calendar view without the events
+        //      and ask the user to reload the page
+        
         res.render(successPath, {username: req.session.uname})
     } else {
         console.log("mainController: User is not authenticated")
